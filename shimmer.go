6aa594c079dade786a09f1d8227fc42b93c1c84f
Agniva De Sarker <agnivade@yahoo.co.in>
2018-06-16 16:03:30 +0530

    moving to applying brightness

--- shimmer.go
+++ shimmer.go
@@ -4,11 +4,15 @@ import (
 	"bytes"
 	"encoding/base64"
 	"fmt"
-	"image/jpeg"
-	"image/png"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
 	"io"
 	"strings"
 	"syscall/js"
+
+	"github.com/anthonynsimon/bild/adjust"
+	"github.com/anthonynsimon/bild/imgio"
 )
 
 const jpegPrefix = "data:image/jpeg;base64,"
@@ -24,32 +28,40 @@ func main() {
 		source := js.Global.
 			Get("document").Call("getElementById", "sourceImg").
 			Get("src").String()
-		if strings.HasPrefix(source, jpegPrefix) {
+
+		switch {
+		case strings.HasPrefix(source, jpegPrefix):
 			source = strings.TrimPrefix(source, jpegPrefix)
-		} else {
-			fmt.Println("Unrecognized image format")
+		case strings.HasPrefix(source, pngPrefix):
+			source = strings.TrimPrefix(source, pngPrefix)
+		default:
+			// TODO: log this in the status div
+			fmt.Println("unrecognized image format")
 			return
 		}
+
 		reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(source))
-		convertToPNG(reader)
+		applyTransformation(reader)
 	})
 	js.Global.Get("document").
 		Call("getElementById", "sourceImg").
 		Call("addEventListener", "load", loadImgCb)
-	fmt.Println("hello wasm")
 
 	// Just waiting indefinitely for now
 	select {}
 }
 
-func convertToPNG(r io.Reader) {
-	i, err := jpeg.Decode(r)
+func applyTransformation(r io.Reader) {
+	i, _, err := image.Decode(r)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	var buf bytes.Buffer
-	err = png.Encode(&buf, i)
+	res := adjust.Brightness(i, 0.75)
+
+	enc := imgio.JPEGEncoder(90)
+	err = enc(&buf, res)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -58,5 +70,5 @@ func convertToPNG(r io.Reader) {
 	// Updating the image
 	js.Global.Get("document").
 		Call("getElementById", "targetImg").
-		Set("src", pngPrefix+base64.StdEncoding.EncodeToString(buf.Bytes()))
+		Set("src", jpegPrefix+base64.StdEncoding.EncodeToString(buf.Bytes()))
 }
